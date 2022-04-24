d439ae0fd5a38c6e08c3c7172f2502d53ccd3a27
Christopher-Leung <christopherleung@hotmail.ca>
2022-04-24 00:48:08 -0400

    Adds knn evaluation to evaluate embeddings

--- edge_management/6_evaluate_embeddings.py
+++ edge_management/6_evaluate_embeddings.py
@@ -3,6 +3,7 @@ import csv
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_distances
+from sklearn.neighbors import NearestNeighbors
 
 import weat
 
@@ -63,6 +64,27 @@ def get_weat_score(targets, attributes, embeddings):
 # TODO: p score in the WEAT paper
 
 
+ # K-nn 
+def get_knn(target_word, k, old_embeddings, new_embeddings):
+    """Returns a list of words which are the k nearest neighbours to the target word, sorted by distance."""
+    # only get existing indices
+    target_emb = old_embeddings.loc[target_word].to_numpy().reshape(1, -1)
+    nbrs = NearestNeighbors(n_neighbors=k+1, algorithm='ball_tree').fit(old_embeddings)
+    distances, old_indices = nbrs.kneighbors(target_emb)
+
+    target_emb = new_embeddings.loc[target_word].to_numpy().reshape(1, -1)
+    nbrs = NearestNeighbors(n_neighbors=k+1, algorithm='ball_tree').fit(new_embeddings)
+    distances, new_indices = nbrs.kneighbors(target_emb)
+
+    old_neighbours = old_embeddings.iloc[old_indices[0]].copy().iloc[1:]
+    old_neighbours = list(old_neighbours.index)
+    new_neighbours = new_embeddings.iloc[new_indices[0]].copy().iloc[1:]
+    new_neighbours = list(new_neighbours.index)
+    print(f"K-NN for {target_word}")
+    print("Old:", old_neighbours)
+    print("New:", new_neighbours)
+
+
 def run_weat_test(targets, attributes, numberbatch_embeddings, retrofitted_embeddings):
     original_weat_score = get_weat_score(targets, attributes, numberbatch_embeddings)
     retrofitted_weat_score = get_weat_score(targets, attributes, retrofitted_embeddings)
@@ -109,6 +131,13 @@ def analysis(reduced_dimension=False, from_kgc=False):
     print(distance_arr[-20:])
     print('Lowest cosine_distances:')
     print(distance_arr[:20])
+
+    # Find the knn of the words with the lowest 10 cosine distances
+    print()
+    for word in distance_arr[:20].index:
+        get_knn(word[0], 10, numberbatch_df_touched, retrofitted_df_touched)
+        print()
+    print()
 
     # Word Embedding Association Test Demo
     run_weat_test(targets=GENDER,
